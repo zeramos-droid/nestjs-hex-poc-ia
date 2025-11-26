85e3af20be2fea0fd5fdfd82de23c8d3bb22f9a2
Zehiael Marsio Ramos Espinosa <zeramos@hebmex.com>
2025-11-26 09:28:44 -0600

    feat: add DI tokens for product use cases and repository

--- src/application/config/tokens.ts
+++ src/application/config/tokens.ts
@@ -2,6 +2,16 @@ export const HEALTH_TOKENS = {
   GET_HEALTH_USE_CASE: Symbol('IGetHealthUseCase'),
 };
 
+export const PRODUCT_TOKENS = {
+  PRODUCT_REPOSITORY: Symbol('IProductRepository'),
+  CREATE_PRODUCT_USE_CASE: Symbol('ICreateProductUseCase'),
+  GET_PRODUCT_BY_ID_USE_CASE: Symbol('IGetProductByIdUseCase'),
+  GET_PRODUCTS_USE_CASE: Symbol('IGetProductsUseCase'),
+  UPDATE_PRODUCT_USE_CASE: Symbol('IUpdateProductUseCase'),
+  DELETE_PRODUCT_USE_CASE: Symbol('IDeleteProductUseCase'),
+  UPDATE_STOCK_USE_CASE: Symbol('IUpdateStockUseCase'),
+};
+
 export const INFRASTRUCTURE_TOKENS = {
   ENVIRONMENT_SERVICE: Symbol('IEnvironmentService'),
 };
