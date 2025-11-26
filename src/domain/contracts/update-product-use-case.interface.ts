b37b950e27f6fffcba43680f430187961c6aff96
Zehiael Marsio Ramos Espinosa <zeramos@hebmex.com>
2025-11-26 09:34:24 -0600

    fix: rename IUpdateProductDTO to IUpdateProductInput to avoid naming conflict

--- src/domain/contracts/update-product-use-case.interface.ts
+++ src/domain/contracts/update-product-use-case.interface.ts
@@ -2,10 +2,10 @@ import { IUseCase } from './use-case.interface';
 import { IUpdateProductDTO } from './dtos/update-product.dto';
 import { IProductResponseDTO } from './dtos/product-response.dto';
 
-export interface IUpdateProductDTO {
+export interface IUpdateProductInput {
   id: string;
   data: IUpdateProductDTO;
 }
 
 export interface IUpdateProductUseCase
-  extends IUseCase<IUpdateProductDTO, IProductResponseDTO> {}
+  extends IUseCase<IUpdateProductInput, IProductResponseDTO> {}
